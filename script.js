5f98c6643b4ec621034ac9e1764ea3bd5cbca63d
sasnar <juugotempin@yandex.ru>
2024-02-19 13:57:37 +0200

    Added some styling to the cointainer, added functions for digit input and operands, need to implement equality and clear buttons properly

--- script.js
+++ script.js
@@ -14,24 +14,43 @@ function divide (a,b) {
     return a / b;
 }
 
-let firstNumber;
-let secondNumber;
-let operator;
 
-function operate (firstNumber, secondNumber, operator) {
-    switch (operator) {
-        case "+": 
-            return add(firstNumber, secondNumber);
-        case "-":
-            return subtract(firstNumber, secondNumber);
-        case "*":
-            return multiply(firstNumber, secondNumber);
-        case "/":
-            return divide(firstNumber, secondNumber);
-    }
-}
 
 function calculator() {
+    let firstNumber;
+    let secondNumber;
+    let operator;
+    
+    function operate (firstNumber, secondNumber, operator) {
+        switch (operator) {
+            case "+": 
+                return add(firstNumber, secondNumber);
+            case "-":
+                return subtract(firstNumber, secondNumber);
+            case "*":
+                return multiply(firstNumber, secondNumber);
+            case "/":
+                if(firstNumber == 0 || secondNumber == 0) {
+                    display.textContent = "You cannot divide by zero!";
+                    for (let i = 0; i < digits.length; i ++) {
+                        digits[i].disabled = true;
+                    }
+                    for (let i = 0; i < operands.length; i++) {
+                        operands[i].disabled = true;
+                    }
+                } else {
+                    let result = divide(firstNumber, secondNumber);
+                    if (result % 1 == 0) {
+                        return result;
+                    } else {
+                        return Math.round(result * 100) / 100;
+                    }
+                }
+            case "=":
+                flag = false;
+        }
+    }
+
     let displayValue = [];
     let digits = document.querySelectorAll('.digit');
     let display = document.querySelector('.display');
@@ -43,15 +62,34 @@ function calculator() {
     });
 
     let buffer;
+    let result;
     let operands = document.querySelectorAll('.operand');
+    let flag = false; 
+    let bufferOperator;
+    let clear = document.querySelector('.clear');
     operands.forEach(function (operand) {
         operand.addEventListener('click', function() {
-            if (!buffer) {
+            if (flag == false) {
                 buffer = +displayValue.join("");
-                console.log(typeof(buffer));
+                displayValue.length = 0;
+                display.textContent = buffer;
+                bufferOperator = operand.textContent;
             }
+            if (flag == true) {     
+                result = operate(buffer, +displayValue.join(""),bufferOperator);
+                display.textContent = result;
+                displayValue.length = 0;
+                buffer = result;
+                bufferOperator = operand.textContent;
+            }
+            flag = true;
         })
     });
+    clear.addEventListener('click', function () {
+        window.location.reload();
+    });
+
+
 }
 
 calculator();
